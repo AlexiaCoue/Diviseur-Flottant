445cfa2e0b913d40d1bc515ffcacdae39563d5dc
etudiant <etudiant@g141-debian>
2015-01-26 14:00:51 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace.std;
+
+int main()
+{
+float total, nombre1, nombre2;
+
+cout << "Nombre1? " << endl;
+cin >> nombre1;
+
+cout << "Nombre2? " << endl;
+cin >> nombre2;
+
+total = nombre1 / nombre2;
+
+cout << "Total : " << total << endl;
+
+return 0;
+}
+
+
